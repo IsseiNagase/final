438d82a4791e6621331772a9dc67a8f136bbb319
Issei Nagase <e235732@ie.u-ryukyu.ac.jp>
2024-01-21 16:53:40 +0900

    テスト

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235732/App.java
@@ -0,0 +1,15 @@
+
+package jp.ac.uryukyu.ie.e235732;
+import java.util.Scanner;
+/**
+ * App
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("入力してください");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+
+    }
+}
